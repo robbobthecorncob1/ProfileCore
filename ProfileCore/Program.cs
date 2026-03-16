042b9e6e6bbc7fab5483464719a086ddd30ca1b4
Adam Hilty <hilty.89@osu.edu>
2026-03-15 22:45:49 -0400

    Registered GpaCalculatorService interface and class to DI

--- ProfileCore/Program.cs
+++ ProfileCore/Program.cs
@@ -1,3 +1,5 @@
+using ProfileCore.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<IGpaCalculatorService, GpaCalculatorService>();
 
 var app = builder.Build();
 
