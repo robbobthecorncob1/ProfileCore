1c5df8b1e7657b4a57f4a4e606ca6056f7badc24
Adam Hilty <hilty.89@osu.edu>
2026-03-16 21:38:27 -0400

    Added GPA target and response records

--- ProfileCore/Models/GpaCalculator.cs
+++ ProfileCore/Models/GpaCalculator.cs
@@ -33,3 +33,27 @@ public record GpaCalculationResponse(
     double CalculatedGpa,
     string Message
 );
+
+/// <summary>
+/// Represents the incoming payload from the client containing the data needed to calculate the minimum GPA.
+/// </summary>
+/// <param name="CurrentGpa">The user's current GPA number</param>
+/// <param name="PastCreditHours">The number of credit hours the user has taken in the past.</param>
+/// <param name="TargetGpa">The user's target GPA number to achieve.</param>
+/// <param name="NewCreditHours">The next block of credit hours the student will complete.</param>
+public record TargetGpaRequest(
+    double CurrentGpa,
+    double PastCreditHours,
+    double TargetGpa,
+    double NewCreditHours
+);
+
+/// <summary>
+/// The final result of a GPA calculation, containing the required minimum GPA to the client.
+/// </summary>
+/// <param name="RequiredGpa">The computed Grade Point Average.</param>
+/// <param name="Message">A message containing information about the calculation.</param>
+public record TargetGpaResponse(
+    double RequiredGpa,
+    string Message
+);
