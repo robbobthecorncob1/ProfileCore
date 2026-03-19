09247f1875d4df910ebd70a33ec8a8115cd7ffc2
Adam Hilty <hilty.89@osu.edu>
2026-03-19 16:37:08 -0400

    Migrations to create schema

---
+++ ProfileCore/Migrations/20260319203206_InitialCreate.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProfileCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Education",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    IsComplete = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Degree = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Major = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    School = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    College = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    Gpa = table.Column<double>(type: "REAL", nullable: true),
+                    DateStarted = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    DateEnded = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 20000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Education", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jobs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    IsCurrentJob = table.Column<bool>(type: "INTEGER", nullable: true),
+                    Position = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Company = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Technologies = table.Column<string>(type: "TEXT", nullable: false),
+                    DateStarted = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    DateEnded = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 20000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jobs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profile",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Headline = table.Column<string>(type: "TEXT", maxLength: 2000, nullable: false),
+                    Bio = table.Column<string>(type: "TEXT", maxLength: 20000, nullable: false),
+                    ResumeUrl = table.Column<string>(type: "TEXT", maxLength: 500, nullable: true),
+                    Skills = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profile", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    IsComplete = table.Column<bool>(type: "INTEGER", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Technologies = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 20000, nullable: false),
+                    RepoURL = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    DateStarted = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    DateEnded = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SocialLink",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Platform = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Url = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    ProfileId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SocialLink", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SocialLink_Profile_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profile",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SocialLink_ProfileId",
+                table: "SocialLink",
+                column: "ProfileId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Education");
+
+            migrationBuilder.DropTable(
+                name: "Jobs");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "SocialLink");
+
+            migrationBuilder.DropTable(
+                name: "Profile");
+        }
+    }
+}
